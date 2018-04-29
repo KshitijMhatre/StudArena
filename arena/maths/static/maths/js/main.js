0d46c06a308061a615498ad734aa8ccc42a1e175
KshitijMhatre <kshitijpmhatre@gmail.com>
2018-04-29 12:36:14 +0530

    prototype

---
+++ arena/maths/static/maths/js/main.js
@@ -0,0 +1,35 @@
+
+// var playPause = anime({
+//     targets:'div.box',
+//     translateY:[
+//         { value: 200, duration: 500 },
+//         { value: 0, duration: 800 }
+//     ],
+//     rotate:{
+//         value:'1turn',
+//         easing: 'easeInOutSine'
+//     },
+//     delay:function(element,i,l){
+//         return i*1000;
+//     },
+//     autoplay:false,    
+// });
+
+// $('#red').click(
+//     $('#resp').val( $('#resp').val() + $('#red').val() )
+// );
+
+// $('#blue').click(
+//     $('#resp').val($('#resp').val()+$('#blue').val())    
+// );
+
+// $('#green').click(
+//     $('#resp').val($('#resp').val()+$('#green').val())    
+// );
+
+// $('#yellow').click(
+//     $('#resp').val($('#resp').val()+$('#yellow').val())
+// );
+
+
+
